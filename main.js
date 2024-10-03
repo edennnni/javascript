bf590e31ae7b350f4a3b6185ab23639c108fb998
edennnni <edenkurtolli7@gmail.com>
2024-10-03 16:23:54 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var name="EDENNNNNNI"
+var age= 16.9999999999999999999999999999999
+var truth= false
+
+var a = 50 + 30 //sum
+var b = 30 - 15 //difference
+var c = 120/17 //pjestimi
+var d = 30 % 7 //modulo
+var e = 15 * 30 //shumezimi
+var f = 3**2 //fuqia
+
+console.log(a) // Printing the value of the variable
+
+console.log(b)
+
+console.log(c)
+
+console.log(d)
+
+console.log(e)
+
+console.log(f)
+
+a+=15
+console.log(a)
+
+a++;
+
+console.log(a)
+  
+var numri1 = "20"
+var numri2 = 20
+
+console.log(numri1 ==numri2)
+console.log(numri === numri2)
+
+console.log(!(numri1 === numri2))
