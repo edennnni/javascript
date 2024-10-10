da180faec5f432c3323ddb930b44ef91e315b56f
edennnni <110981975+edennnni@users.noreply.github.com>
2024-10-10 16:13:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,12 @@
-var name="EDENNNNNNI"
-var age= 16.9999999999999999999999999999999
-var truth= false
+function showMessage() {
+    alert ("this message is inside the function");
+}
+showMessage
 
-var a = 50 + 30 //sum
-var b = 30 - 15 //difference
-var c = 120/17 //pjestimi
-var d = 30 % 7 //modulo
-var e = 15 * 30 //shumezimi
-var f = 3**2 //fuqia
+function sum*(number1, number2)*{
+    return number1 + number2;
+}
+console.log(sum(25.5));
 
-console.log(a) // Printing the value of the variable
-
-console.log(b)
-
-console.log(c)
-
-console.log(d)
-
-console.log(e)
-
-console.log(f)
-
-a+=15
-console.log(a)
-
-a++;
-
-console.log(a)
-  
-var numri1 = "20"
-var numri2 = 20
-
-console.log(numri1 ==numri2)
-console.log(numri === numri2)
-
-console.log(!(numri1 === numri2))
+var result = toCelsius(54)
+console.log ("54 fahrenheit is equal to "+result+"celsius")
