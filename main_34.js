631da4613efd6a6489e49c86cc13a7d85f85d64d
edennnn <edenkurtolli7@gmail.com>
2024-11-26 16:20:14 +0100

    module

--- main_34.js
+++ main_34.js
@@ -6,3 +6,28 @@ console.log("Edenii");
 }
 
 printNames();
+function search(){
+var input, ul, li, x, i, txtValue, filter;
+input = document.getElementById('searchInput');
+filter = input.value.toUpperCase();
+ul = document.getElementById('ListEl');
+for(i=0; i<li.length; i++){
+x = li [i];
+txtValue = x.textContent;
+if(txtValue.toUpperCase().indexOf(filter)>-1){
+li[i].style.display = 'block';
+}
+else{
+    li[i].style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+}
