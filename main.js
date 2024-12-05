81c88ea10d8a694aa94d9266bbea01044ccd891e
edennnni <110981975+edennnni@users.noreply.github.com>
2024-12-05 16:13:57 +0100

    prjkt

---
+++ main.js
@@ -0,0 +1,30 @@
+$ ("li").click(function()){
+alert("This was clicked");
+})
+
+
+$("#btn").click(function(){
+$("#h1").text("anik dhe edeni")
+$("#h1").append("dhe eden") 
+}
+
+$("#div1").click(function(){
+$(this).addClass("clicked");
+}).find("span".padStart("title", "this is a changed title");
+
+$("btnShow").click(function()){
+$("hidden").show(500);
+}
+
+$("#btnhide").click(function(){
+$ ("#hidden").hide();
+});
+
+$(".animate").click(function()){
+$(this).animate({
+"opacity" : "-=0,1",
+"left" : "+=50px";
+
+
+
+)}
